c2f4f102117c6bb45306fdd861e151eba0978c57
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-30 16:14:09 +0300

    added files

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+import string
+
+
+class Movie:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self, id, title, overview, poster, vote_average, vote_count, tagline, genres, date, language, prod, imdb_id, runtime):
+        self.id = id
+        self.title = title
+        self.overview = overview
+        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+        self.tagline = tagline
+        self.genres = genres
+        self.date = date
+        self.language = language
+        self.prod = prod
+        self.imdb_id = imdb_id
+        self.runtime = runtime
+
+class Trailer:
+    def __init__(self,id,name,key,type):
+        self.id = id
+        self.name = name
+        self.key = key
+        self.type = type
--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,34 @@
+from unittest import result
 from django.shortcuts import render
 from django.http import HttpResponse
+from .request import get_movies, search_movie
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    movies = get_movies('popular')
+
+    
+    
+    return render(request, 'index.html',{'movies':movies})
+
+# def search(request):
+#     if 'title' in request.GET and request.GET["title"]:
+#         mname = request.GET.get('title')
+#         result = search_movie('mname')
+#         message = f"{mname}"
+#         for i in result:
+#             print(i)
+        
+#         return render(request, 'search.html',{'movies':result,'message':message})
+        
+#     else:
+#         message = 'no movie'
+#         return render(request, 'search.html',{'message':message})
+    
+    
+
+
+
+
+
